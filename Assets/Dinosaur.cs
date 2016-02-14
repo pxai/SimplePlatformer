9f507be0f133fb529c9473c4db2d0bac96f405ba
pxai <pello_altadill@cuatrovientos.org>
2016-02-15 00:39:17 +0100

    Ok, first commit

---
+++ Assets/Dinosaur.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dinosaur : MonoBehaviour {
+	private int speed= 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log ("Space was pressed");
+			this.GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+		}
+	}
+}
