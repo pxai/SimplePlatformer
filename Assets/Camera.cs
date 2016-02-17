80a3f061c6cb138c530958cd3305448e2cba1543
pxai <pello_altadill@cuatrovientos.org>
2016-02-17 19:53:25 +0100

    some changes added

---
+++ Assets/Camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject player;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector3 playerpos = player.transform.position;
+        playerpos.z = transform.position.z;
+        transform.position = playerpos;
+    }
+}
---
+++ Assets/Character.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log ("Space was pressed");
+			this.GetComponent<Rigidbody2D>().velocity = Vector2.up * 5;
+		}
+
+		if (Input.GetKeyDown ("a")) {
+			this.GetComponent<Rigidbody2D>().velocity = Vector2.left * 2;
+		}
+
+		if (Input.GetKeyDown ("d")) {
+			this.GetComponent<Rigidbody2D>().velocity = Vector2.right * 2;
+		}
+		this.transform.rotation = Quaternion.identity;
+	}
+}
